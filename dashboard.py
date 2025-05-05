3d032a24be395eacff601933f398f6c9cd2000ec
Selenge Tulga <49024084+selengetu@users.noreply.github.com>
2025-05-05 16:28:22 -0500

    changes

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
 
-st.set_page_config(page_title="Infinity BH Dashboards", layout="wide")
+# st.set_page_config(page_title="Infinity BH Dashboards", layout="wide")
 
 def show_dashboard():
     
@@ -1482,5 +1482,5 @@ def show_dashboard():
         unsafe_allow_html=True
     )    
 
-if __name__ == "__main__":
-    show_dashboard()
+# if __name__ == "__main__":
+#     show_dashboard()
